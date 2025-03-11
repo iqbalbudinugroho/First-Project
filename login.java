44898249362f371123c71b5524d49810e87f2711
iqbalbudinugroho <iqbalbudinugroho@gmail.com>
2025-03-11 19:47:50 -0400

    commit java

--- login.java
+++
@@ -1,7 +0,0 @@
-public class login {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your username: ");
-        String username = sc.nextLine();
-    }
-}
